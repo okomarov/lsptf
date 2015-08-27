9ed7d691e02e646f6c73baba3305e984d426ea40
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-08-27 19:19:22 +0100

    binIndependently with recursive implementation

--- lsptf/mapSignal.py
+++ lsptf/mapSignal.py
@@ -1,19 +1,52 @@
 import numpy as np
 
-def binData(*args, IndipendentSort=True, NumBins=10, Percentiles=None, Edges=None):
-    
+def binData(*data, IndependentSort=True, NumBins=10, Percentiles=None, Edges=None):
+
+    # Extract edges info
     if Edges is not None:
-        opts = {'edges':Edges}
+        opts = {'edges': Edges,
+                'nbins': Edges.shape[1]}
     elif Percentiles is not None:
-        opts = {'ptiles':Percentiles}
+        opts = {'ptiles': Percentiles,
+                'nbins' : Percentiles.shape[1]}
     else:
-        opts = {'ptiles':np.linspace(0,100, NumBins+1)}
-    
-    
-    if IndipendentSort:
-        bins = _binIndipendently(*args, **opts)
+        opts = {'ptiles': np.linspace(0, 100, NumBins+1),
+                'nbins' : NumBins}
+
+    # Bin
+    if IndependentSort or len(data) == 1:
+        bins = _binIndependently(*data, **opts)
     else:
-        bins = _binConditionally(*args, **opts)
+        bins = _binConditionally(None,*data, **opts)
+
+    return bins
+
+
+def _binIndependently(*data, **opts):
+    return np.squeeze([_binCore(x, **opts) for x in data])
+
+
+def _binConditionally(binmask,*data, **opts):
+    """
+    Bin within bins, conditioning on previous run. Recursive implementation
+    """
+
+    sz = (len(data),) + data[0].shape
+    # First run
+    if binmask is None:
+        bins    = np.zeros(sz, np.int64)
+        bins[0] = _binCore(data[0], **opts)
+    else:
+        # Bin on previously conditioned
+        tmp  = np.ma.array(data[0], mask=~binmask, fill_value=np.nan)
+        bins = _binCore(tmp.filled(), **opts)
+
+    # Recurse if next level is available
+    if sz[0] > 1:
+        # Condition on each value of current binning
+        for v in np.arange(opts['nbins'])+1:
+            binmask          = bins[0] == v
+            bins[1, binmask] = _binConditionally(binmask,*data[1:], **opts)[binmask]
 
     return bins
 
