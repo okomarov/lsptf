dbc527e2a4fcd6ef14ccf434855c1b1e53460cf6
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-08-27 19:16:34 +0100

    looped percentile instead of nanpercentile to avoid https://github.com/numpy/numpy/issues/5760

--- lsptf/mapSignal.py
+++ lsptf/mapSignal.py
@@ -17,29 +17,27 @@ def binData(*args, IndipendentSort=True, NumBins=10, Percentiles=None, Edges=Non
 
     return bins
 
-def _binIndipendently(*data, **opts):
-    return [_binCore(x, **opts) for x in data]
+def _binCore(data, ptiles=None, edges=None, nbins=None):
 
-def _binConditionally(*data, **opts):
-    pass
+    nanmask = np.isnan(data)
 
-def _binCore(data, ptiles=None, edges=None):
-    
     if edges is None:
         # Ensure monotonically increasing percentiles, i.e. convert data to 
         # integer ranks. Applied by rows
-        nanmask       = np.isnan(data)
         data          = data.argsort().argsort()
         data          = data.astype(float)
         data[nanmask] = np.nan
-        edges         = np.nanpercentile(data,ptiles,axis=1)
-    
+
     # Return bin to which data belongs
-    bins = np.zeros(data.shape,np.int64)
-    #bins = np.array([np.digitize(data[r,:], edges[r,:]) for r in np.arange(data.shape[0]) if any(~nanmask[r,:])]) 
-    for r in np.arange(data.shape[0]):
-        notnan = ~nanmask[r,:]
-        if any(notnan):
-            bins[r,notnan] = np.digitize(data[r,notnan], edges[r,:])
+    bins = np.zeros(data.shape, np.int64)
+    for r in np.arange(data.shape[0])[np.any(~nanmask,axis=1)]:
+        notnan = ~nanmask[r, :]
+        if edges is None:
+            # TODO: add matlab compatible percentile function? Note that matlab uses midpoint interpolation for all but the extreme percentiles!
+            edge = np.percentile(data[r, notnan], ptiles)
+        else:
+            edge = edges[r, :]
+
+        bins[r, notnan] = np.digitize(data[r, notnan], edge)
     
     return bins
