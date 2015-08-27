923067f8b1d3bb95dd8f50ceb314e462bc765e35
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-08-27 20:31:13 +0100

    save output for matlab comparison

--- lsptf/test.py
+++ lsptf/test.py
@@ -6,13 +6,16 @@ Created on 20 Aug 2015
 import numpy as np
 from mapSignal import binData 
 
-m = 10
-n = 30
+m = 1000
+n = 200
 c = m*n*0.1
 
-signal = np.random.randn(m,n)
+signal = np.random.randn(m, n)
 linidx = np.random.choice(signal.size, c, replace=False)
-np.put(signal,linidx,np.nan)
+np.put(signal, linidx, np.nan)
+np.savetxt("signal.csv", signal, delimiter=",")
 
-out = binData(signal,signal)
-out = np.array(out)
+out = binData(signal,signal, IndependentSort=False)
+
+np.savetxt("out1.csv", out[0], delimiter=",")
+np.savetxt("out2.csv", out[1], delimiter=",")
